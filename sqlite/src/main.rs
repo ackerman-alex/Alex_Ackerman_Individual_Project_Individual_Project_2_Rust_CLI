271e6f8326b919b1a1cec26f0e47a2b9916dc453
Alex Ackerman <105178749+ackerman-alex@users.noreply.github.com>
2024-10-23 21:46:48 +0000

    added update function

--- sqlite/src/main.rs
+++ sqlite/src/main.rs
@@ -43,8 +43,6 @@ enum Commands {
     },
     ///Pass a table name, set clause, and condition to Update operations
     /// sqlite -u table_name set_clause condition
-    /// Example sqlite -u spotify_tracks "track_name = 'New Track Name', artist = 'New Artist', in_spotify_chart = 150" "music_id = 3"
-
     #[command(alias = "u", short_flag = 'u')]
     Update {
         table_name: String,
